8586b7c73119f9ff7d15572f8f6e515e9694b85f
Akanksha <msa23003@iiitl.ac.in>
2025-01-25 23:25:34 +0530

    Updated file

--- src/models/register_model.py
+++ src/models/register_model.py
@@ -25,12 +25,12 @@ handler.setFormatter(formatter)
 # initialize dagshub
 import dagshub
 import mlflow.client
-dagshub.init(repo_owner='himanshu1703', 
+dagshub.init(repo_owner='msa23003', 
              repo_name='swiggy-delivery-time-prediction', 
              mlflow=True)
 
 # set the mlflow tracking server
-mlflow.set_tracking_uri("https://dagshub.com/himanshu1703/swiggy-delivery-time-prediction.mlflow")
+mlflow.set_tracking_uri("https://dagshub.com/msa23003/swiggy-delivery-time-prediction.mlflow")
 
 
 def load_model_information(file_path):
